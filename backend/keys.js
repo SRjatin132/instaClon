6f53b43120a60aa5c6145fdb3646151fed391aa0
SRjatin132 <jatin.kakadiya@swiftrut.com>
2025-01-04 16:49:13 +0530

    Create Modal and mongoose conect

---
+++ backend/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://mansirakholiya570:mansi70@cluster0.d2pgw.mongodb.net/Insta?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "jkangdvcs"
+
+};
